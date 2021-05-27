bdf40250396f3e0b7de2022c35f6de5077bfc42a
Daniel Kinder <daniel.kinder@stockport.gov.uk>
2021-05-27 15:21:20 +0100

    Spelling Error in Planning Apps layer

--- src/Configuration.js
+++ src/Configuration.js
@@ -62,7 +62,7 @@ const Configuration = {
         },
 
         {
-            key: 'Sites with plannning permission for housing (SHLAA 2020)',
+            key: 'Sites with planning permission for housing (SHLAA 2020)',
             url: 'https://spatial.stockport.gov.uk/geoserver/wfs?service=WFS&version=1.1.0&request=GetFeature&typeName=planning:schema2020_resi_permissioned&outputFormat=application/json&bbox={0},EPSG:4326&srsName=EPSG:4326',
             layerOptions: {
                 onEachFeature: permissionedPopup,
